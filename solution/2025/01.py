00f1fc17974f630d4dd2bf872bc400955e5d025a
Marten Tacoma <marten.tacoma@nioz.nl>
2025-12-01 12:41:01 +0100

    remove last bug from 2025-01 (so also the tweakers large input works)

--- solution/2025/01.py
+++ solution/2025/01.py
@@ -5,37 +5,28 @@ class Solution(Base):
     year = 2025
     day = "01"
     position = 50
-    positions = {0: 0}
-    through = 0
-    
+    at_zero = 0
+    all_zeros = 0
+
     def parse_input(self, data):
         return data.splitlines()
 
     def part1(self):
         for line in self.input:
             self.dial(line)
-        return self.positions[0]
+        return self.at_zero
 
     def part2(self):
-        return self.positions[0] + self.through
+        return self.all_zeros
 
     def dial(self, action):
-        dir = action[0]
-        amount = int(action[1:])
-        if dir == 'R':
-            next = self.position + amount
-            through = math.floor((next - 1) / 100)
-        elif dir == 'L':
-            next = self.position - amount
-            if next < 0:
-                through = math.floor((100 - next) / 100)
-                if self.position == 0 or next % 100 == 0:
-                    through -= 1
-            else:
-                through = 0
-        self.through += through
-        self.position = next % 100
-        if self.position not in self.positions:
-            self.positions[self.position] = 1
-        else:
-            self.positions[self.position] += 1
+        steps = int(action[1:])
+        self.all_zeros += math.floor(steps / 100)
+        delta = (-1 if action[0] == 'L' else 1) * (steps % 100)
+        next_position = self.position + delta
+        if self.position > 0 and not 0 < next_position < 100:
+            self.all_zeros += 1
+        self.position = next_position % 100
+
+        if self.position == 0:
+            self.at_zero += 1
