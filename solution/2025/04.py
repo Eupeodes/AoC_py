87ac230fab007b1cad9b12a261c70ac073903d65
Marten Tacoma <marten.tacoma@nioz.nl>
2025-12-05 09:02:41 +0100

    2025 day 4

---
+++ solution/2025/04.py
@@ -0,0 +1,51 @@
+from solution.base import Base
+
+class Solution(Base):
+    year = 2025
+    day = "04"
+    grid = {}
+
+    def parse_input(self, data):
+        l = 0
+        for line in data.splitlines():
+            self.grid[l] = {i: v for i, v in enumerate(list(line))}
+            l += 1
+
+    def part1(self):
+        output = 0
+        for line, columns in self.grid.items():
+            for column, state in columns.items():
+                if state == '@':
+                    if self._get_neighbours(line, column) < 4:
+                        output += 1
+        return output
+
+    def part2(self):
+        output = 0
+        n = 1
+        while n > 0:
+            n = self._remove_rolls()
+            output += n
+        return output
+
+    def _remove_rolls(self):
+        output = 0
+        for line, columns in self.grid.items():
+            for column, state in columns.items():
+                if state == '@':
+                    if self._get_neighbours(line, column) < 4:
+                        output += 1
+                        self.grid[line][column] = '.'
+        return output
+
+    def _get_neighbours(self, line, column):
+        n = -1
+        for l in [line - 1, line, line + 1]:
+            for c in [column - 1, column, column + 1]:
+                try:
+                    neighbour = self.grid[l][c]
+                except KeyError:
+                    neighbour = '-'
+                if neighbour == '@':
+                    n += 1
+        return n
